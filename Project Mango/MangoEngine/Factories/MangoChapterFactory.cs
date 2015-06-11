17bbb6976c21421704e2e01399b99901a5bf143c
Code-ScottLe <AMRAAMMissiles@hotmail.com>
2015-06-11 12:09:16 +0700

    Done with the factory

--- Project Mango/MangoEngine/Factories/MangoChapterFactory.cs
+++ Project Mango/MangoEngine/Factories/MangoChapterFactory.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MangoEngine.Chapters;
+using MangoEngine.Exceptions;
 
 namespace MangoEngine.Factories
 {
     public class MangoChapterFactory
     {
+        #region Fields
+        /*Fields*/
+        #endregion
+
+        #region Property
+        /*Property*/
+        #endregion
+
+        #region Constructor
+        /*Constructors*/
+        #endregion
+
+        #region Methods
+        /*Methods*/
+
+        public MangoChapter CreateNew(string sourceName, string sourceUrl)
+        {
+            /*Return back the correct instance of the corresponding source type (sync)*/
+            return CreateNewAsync(sourceName, sourceUrl).Result;
+        }
+
+        public async Task<MangoChapter> CreateNewAsync(string sourceName, string sourceUrl)
+        {
+            /*Return back the correct instance of the corresponding source type (async)*/
+            MangoChapter source = null;
+
+            switch (sourceName)
+            {
+                case "Batoto":
+                    source = new BatotoMangoChapter(sourceUrl);
+                    break;              
+            }
+
+            if (source == null)
+            {
+                throw new MangoException("Can't create an instance of  Mango_Source!");
+            }
+
+            //Initalize the source (synced)
+            await source.InitAsync();
+
+            //Done, return the source
+            return source;
+        }
+        #endregion
     }
 }
