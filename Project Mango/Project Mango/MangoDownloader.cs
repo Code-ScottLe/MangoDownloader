458680a4ac006e0f9ebbb1872b415d77a11c3583
Code-ScottLe <AMRAAMMissiles@hotmail.com>
2015-06-12 00:15:34 +0700

    Add in a downloader

---
+++ Project Mango/Project Mango/MangoDownloader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Mango
+{
+    public class MangoDownloader
+    {
+        #region Fields
+        /**/
+        #endregion
+
+        #region Properties
+        /**/
+        #endregion
+
+        #region Constructors
+        /**/
+        #endregion
+
+        #region Methods
+        /**/
+        #endregion
+    }
+}
