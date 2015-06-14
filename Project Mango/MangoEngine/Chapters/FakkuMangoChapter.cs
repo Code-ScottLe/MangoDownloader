782a842ef02cbbf03bb76231ec59eee42f80f506
Code-ScottLe <AMRAAMMissiles@hotmail.com>
2015-06-14 17:41:09 +0700

    Add base code for Fakku Support

---
+++ Project Mango/MangoEngine/Chapters/FakkuMangoChapter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoEngine.Chapters
+{
+    public class FakkuMangoChapter : MangoChapter
+    {
+        #region Fields
+        /*Fields*/
+        #endregion
+
+        #region Properties
+        /*Properties*/
+        #endregion
+
+        #region Constructors
+        /*Constructors*/
+        #endregion
+
+        #region Methods
+        /*Methods*/
+        internal override void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override Task InitAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool NextPage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<bool> NextPageAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetImageUrl()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<string> GetImageUrlAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
