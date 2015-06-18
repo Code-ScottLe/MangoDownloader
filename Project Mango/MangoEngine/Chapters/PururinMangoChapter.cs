6d8c42e92db95cf9127fca870aaab09f5a81661e
Code-ScottLe <amraammissiles@hotmail.com>
2015-06-18 23:45:42 +0700

    Update Pururin with more directed Try-catch practice.

--- Project Mango/MangoEngine/Chapters/PururinMangoChapter.cs
+++ Project Mango/MangoEngine/Chapters/PururinMangoChapter.cs
@@ -61,57 +61,69 @@ namespace MangoEngine.Chapters
 
             //url handler here.
             await urlHandlerAsync();
+
+            //Get the response from the website.
+            HttpResponseMessage responseMessage = null;
             try
             {
-                //Get the response from the website.
-                HttpResponseMessage responseMessage = await myClient.GetAsync(CurrentUrl);
+                responseMessage = await myClient.GetAsync(CurrentUrl);
+            }
 
-                //Set Encoding
-                EncodingType = GetEncoding(responseMessage);
+            catch
+            {
+                throw new MangoException("Init failed! Can't Get Response from Website");
+            }
+                
 
-                //Get a stream to Pururin
-                Stream pururinStream = await myClient.GetStreamAsync(CurrentUrl);
+            //Set Encoding    
+            EncodingType = GetEncoding(responseMessage);
 
-                //Async-Wrapper for Parsing process
-                await Task.Run(() =>
+            //Get a stream to Pururin    
+            Stream pururinStream = null;
+            try
+            {
+                pururinStream = await myClient.GetStreamAsync(CurrentUrl);
+            }
+
+            catch
+            {
+                throw new MangoException("Init failed! Can't get Stream to the Website");
+            }
+
+            //Async-Wrapper for Parsing process    
+            await Task.Run(() =>    
+            {    
+                /*Load the Stream up as HTML*/    
+                HtmlDocument pururinDocument = new HtmlDocument();    
+                pururinDocument.Load(pururinStream, EncodingType);    
+
+                /*Pururin has the number of pages insid ehte select node with the id=image-pageSelect*/    
+                HtmlNode pageSelectNode = pururinDocument.DocumentNode.SelectSingleNode("//select[@class=\"image-pageSelect\"]");
+
+                if (pageSelectNode == null)
                 {
-                    /*Load the Stream up as HTML*/
-                    HtmlDocument pururinDocument = new HtmlDocument();
-                    pururinDocument.Load(pururinStream, EncodingType);
+                    throw new MangoException("Init Failed! Can't find the pageSelectNode");
+                }    
 
-                    /*Pururin has the number of pages insid ehte select node with the id=image-pageSelect*/
-                    HtmlNode pageSlectNode = pururinDocument.DocumentNode.SelectSingleNode("//select[@class=\"image-pageSelect\"]");
+                //The select node has the data-max attribute as the last index of the last page (count from 0);    
+                HtmlAttribute datamaxAttribute = pageSelectNode.Attributes["data-max"];    
 
-                    //The select node has the data-max attribute as the last index of the last page (count from 0);
-                    HtmlAttribute datamaxAttribute = pageSlectNode.Attributes["data-max"];
+                //parse the value for the interger    
+                int numPages = 0;    
 
-                    //parse the value for the interger
-                    int numPages = 0;
+                if (!Int32.TryParse(datamaxAttribute.Value, out numPages))    
+                {    
+                //if faulted.    
+                throw new MangoException("Init failed! Can't convert number of total pages!");    
+                }    
 
-                    if (!Int32.TryParse(datamaxAttribute.Value, out numPages))
-                    {
-                    //if faulted.
-                    throw new MangoException("can't convert number of total pages!");
+                //set the number of pages (plus one due to the index are 0-based)    
+                PagesCount = numPages + 1;    
 
-                    }
+            });
 
-                    //set the number of pages (plus one due to the index are 0-based)
-                    PagesCount = numPages + 1;
-
-                });
-
-            }
-
-            catch (Exception e)
-            {
-                throw new MangoException("Intialize Failed!", e);
-            }
-
-            finally
-            {
-                //Done with everything, dispose the client
-                myClient.Dispose();
-            }
+            //done with the client
+            myClient.Dispose();
 
         }
         public override string GetImageUrl()
@@ -129,52 +141,47 @@ namespace MangoEngine.Chapters
             //Set the timeout of the client to 30 secs
             myClient.Timeout = new TimeSpan(0, 0, 30);
 
+            //Get the Stream to the website.    
+            Stream pururinStream = null;
             try
             {
-                //Get the Stream to the website.
-                Stream pururinStream = await myClient.GetStreamAsync(CurrentUrl);
+                pururinStream = await myClient.GetStreamAsync(CurrentUrl);
+            }
+                 
+            catch
+            {
+                throw new MangoException("GetImageUrl failed! Can't get Stream to the website!");
+            }
 
-                string PartialimgUrl = await Task.Run<string>(() =>
-                {
-                    //Load it up as a HTML document
-                    HtmlDocument pururinDocument = new HtmlDocument();
-                    pururinDocument.Load(pururinStream);
+            string PartialimgUrl = await Task.Run<string>(() =>    
+            {    
+                //Load it up as a HTML document    
+                HtmlDocument pururinDocument = new HtmlDocument();    
+                pururinDocument.Load(pururinStream);    
 
-                    /*Pururin has the img link embedded inside a img node with class ="b",
-                    We will get the next link while grabbing the IMG url because they are next to each other.
-                    So we don't have to do the GetStream twice.*/
+                /*Pururin has the img link embedded inside a img node with class ="b",    
+                We will get the next link while grabbing the IMG url because they are next to each other.    
+                So we don't have to do the GetStream twice.*/    
 
-                    HtmlNode imgNode = pururinDocument.DocumentNode.SelectSingleNode("//img[@class=\"b\"]");
-                    //Check if the node is valid
-                    if (imgNode == null)
-                    {
-                        throw new MangoException("Can't find the img comic_page link!");
-                    }
+                HtmlNode imgNode = pururinDocument.DocumentNode.SelectSingleNode("//img[@class=\"b\"]");    
+                //Check if the node is valid    
+                if (imgNode == null)    
+                {    
+                    throw new MangoException("GetImageUrl failed! Can't find the img comic_page link!");    
+                }    
 
-                    HtmlNode ImageNextNode = pururinDocument.DocumentNode.SelectSingleNode("//a[@class=\"image-next\"]");
+                HtmlNode ImageNextNode = pururinDocument.DocumentNode.SelectSingleNode("//a[@class=\"image-next\"]");    
 
-                    //only update if the link is valid/there
-                    if (ImageNextNode != null && !string.IsNullOrEmpty(ImageNextNode.Attributes["href"].Value))
-                    {
-                        _nextPageUrl = _pururinPrefix + ImageNextNode.Attributes["href"].Value;
-                    }
+                //only update if the link is valid/there    
+                if (ImageNextNode != null && !string.IsNullOrEmpty(ImageNextNode.Attributes["href"].Value))    
+                {    
+                    _nextPageUrl = _pururinPrefix + ImageNextNode.Attributes["href"].Value;    
+                }    
                     
-                    return imgNode.Attributes["src"].Value;
-                });
+                return imgNode.Attributes["src"].Value;    
+            });    
 
-                return _pururinPrefix + PartialimgUrl;
-            }
-
-            catch (Exception e)
-            {
-                throw new MangoException("Can't get the IMG link!", e);
-            }
-
-            finally
-            {
-                //All done, dispose the client
-                myClient.Dispose();
-            }
+            return _pururinPrefix + PartialimgUrl;    
 
         }
 
