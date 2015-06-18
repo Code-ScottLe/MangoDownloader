56a506bc27f6be2df5af0f732262566dd33ab19f
Code-ScottLe <amraammissiles@hotmail.com>
2015-06-18 17:12:35 +0700

    Done with Pururin Init

--- Project Mango/MangoEngine/Chapters/PururinMangoChapter.cs
+++ Project Mango/MangoEngine/Chapters/PururinMangoChapter.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.IO;
 using MangoEngine.Exceptions;
 using HtmlAgilityPack;
 
@@ -27,12 +28,72 @@ namespace MangoEngine.Chapters
         /*Methods*/
         internal override void Init()
         {
-            throw new NotImplementedException();
+            InitAsync().Wait();
         }
 
-        internal override Task InitAsync()
+        internal async override Task InitAsync()
         {
-            throw new NotImplementedException();
+            /*Initialize the current instance asynchronously*/
+
+            //Create the Client to get stuffs from the website
+            HttpClient myClient = new HttpClient();
+
+            //Set the timeout of the client to 30 secs
+            myClient.Timeout = new TimeSpan(0, 0, 30);
+
+            //url handler here.
+            try
+            {
+                //Get the response from the website.
+                HttpResponseMessage responseMessage = await myClient.GetAsync(CurrentUrl);
+
+                //Set Encoding
+                EncodingType = GetEncoding(responseMessage);
+
+                //Get a stream to Pururin
+                Stream pururinStream = await myClient.GetStreamAsync(CurrentUrl);
+
+                //Async-Wrapper for Parsing process
+                await Task.Run(() =>
+                {
+                    /*Load the Stream up as HTML*/
+                    HtmlDocument pururinDocument = new HtmlDocument();
+                    pururinDocument.Load(pururinStream, EncodingType);
+
+                    /*Pururin has the number of pages insid ehte select node with the id=image-pageSelect*/
+                    HtmlNode pageSlectNode = pururinDocument.DocumentNode.SelectSingleNode("//select[@class=\"image-pageSelect\"]");
+
+                    //The select node has the data-max attribute as the last index of the last page (count from 0);
+                    HtmlAttribute datamaxAttribute = pageSlectNode.Attributes["data-max"];
+
+                    //parse the value for the interger
+                    int numPages = 0;
+
+                    if (!Int32.TryParse(datamaxAttribute.Value, out numPages))
+                    {
+                    //if faulted.
+                    throw new MangoException("can't convert number of total pages!");
+
+                    }
+
+                    //set the number of pages
+                    PagesCount = numPages;
+
+                });
+
+            }
+
+            catch (Exception e)
+            {
+                throw new MangoException("Intialize Failed!", e);
+            }
+
+            finally
+            {
+                //Done with everything, dispose the client
+                myClient.Dispose();
+            }
+
         }
         public override string GetImageUrl()
         {
@@ -52,6 +113,11 @@ namespace MangoEngine.Chapters
         public override Task<bool> NextPageAsync()
         {
             throw new NotImplementedException();
+        }
+
+        private void url_handler()
+        {
+            //Handle the case where the user might choose something elser rather than the chapter page.           
         }
 
         #endregion
