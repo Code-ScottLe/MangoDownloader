6522d8e5dfc7d530a56dc807be7fa95db611dbf6
Code-ScottLe <amraammissiles@hotmail.com>
2015-06-19 23:25:10 +0700

    Re-Add retry class

---
+++ Project Mango/MangoDownloader/Retry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoDownloader
+{
+    /*Code from http://stackoverflow.com/questions/1563191/c-sharp-cleanest-way-to-write-retry-logic */
+    public static class Retry
+    {
+        public static void Do(Action action, TimeSpan retryInterval, int retryCount = 3)
+        {
+            Do<object>(() =>
+            {
+                action();
+                return null;
+            }, retryInterval, retryCount);
+        }
+
+        public static T Do<T>(Func<T> action, TimeSpan retryInterval, int retryCount = 3)
+        {
+            var exceptions = new List<Exception>();
+
+            for (int retry = 0; retry < retryCount; retry++)
+            {
+                try
+                {
+                    return action();
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                    Task.Delay(retryInterval);
+                }
+            }
+
+            throw new AggregateException(exceptions);
+        }
+    }
+}
