b91e660c4bf7c3ec68c6b4360c5500ef5c018606
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-08-13 15:42:21 +0700

    Fields and Property for the MangaChapter

--- ProjectMangoV3/MangoEngine/Chapters/MangaChapter.cs
+++ ProjectMangoV3/MangoEngine/Chapters/MangaChapter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MangoEngine.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +9,97 @@ namespace MangoEngine.Chapters
 {
     public class MangaChapter
     {
+        /*Represent a chapter of a manga*/
+
         #region Fields;
         /*Fields*/
+
+        //metadata
+        private string _sourceName;
+        private string _mangaTitle;
+        private string _chapterTitle;
+        private int _pagesCount;
+        private string _baseUrl;
+
+        //custom data
+        private List<MangaPage> _pages;
+
         #endregion
 
         #region Property
         /*Property*/
+        public string SourceName
+        {
+            get
+            {
+                return _sourceName;
+            }
+
+            protected set
+            {
+                _sourceName = value;
+            }
+        }
+
+        public string MangaTitle
+        {
+            get
+            {
+                return _mangaTitle;
+            }
+
+            protected set
+            {
+                _mangaTitle = value;
+            }
+        }
+
+        public string ChapterTitle
+        {
+            get
+            {
+                return _chapterTitle;
+            }
+
+            protected set
+            {
+                _chapterTitle = value;
+            }
+        }
+
+        public int PageCount
+        {
+            get
+            {
+                return _pagesCount;
+            }
+
+            protected set
+            {
+                _pagesCount = value;
+            }
+        }
+
+        public string BaseUrl
+        {
+            get
+            {
+                return _baseUrl;
+            }
+
+            protected set
+            {
+                _baseUrl = value;
+            }
+        }
+
+        public List<MangaPage> Pages
+        {
+            get { return _pages; }
+            protected set { _pages = value; }
+        }
+
+
         #endregion
 
         #region Constructors
