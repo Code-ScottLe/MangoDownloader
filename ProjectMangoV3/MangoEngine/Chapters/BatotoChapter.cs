2afa9d7d5fe03b6bdb2287fca300acaee1d9efda
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-08-13 20:22:20 +0700

    Done with Init for batoto.

--- ProjectMangoV3/MangoEngine/Chapters/BatotoChapter.cs
+++ ProjectMangoV3/MangoEngine/Chapters/BatotoChapter.cs
@@ -5,7 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using AngleSharp;
 using AngleSharp.Parser.Html;
+using AngleSharp.Dom;
+using AngleSharp.Dom.Html;
 using System.IO;
+using System.Net.Http;
+using MangoEngine.Helpers;
+using MangoEngine.Exceptions;
+using MangoEngine.Pages;
 
 namespace MangoEngine.Chapters
 {
@@ -26,5 +32,71 @@ namespace MangoEngine.Chapters
         #region Methods
         /*Methods*/
         #endregion
+        public override async Task InitAsync()
+        {
+            /*Initialize the current instance of MangaChapter to the Batoto standard.*/
+
+            //Initialize the client for Web purpose
+            HttpClient myClient = new HttpClient();
+
+            //set the timeout of the client (30 secs)
+            myClient.Timeout = new TimeSpan(0, 0, 30);
+
+            //Try to get the stream to the website.
+            Stream batotoStream = null;
+
+            try
+            {
+                batotoStream = await MangoWebHelpers.GetStreamAsync(myClient, BaseUrl, new TimeSpan(0, 0, 3));
+            }
+
+            catch (Exception e)
+            {
+                throw new MangoException("Init failed! Can't get the stream from the website", e);
+            }
+
+            //Parsing part.
+
+            //Initialize the Parser
+            HtmlParser parser = new HtmlParser();
+
+            //Parse the given stream of html
+            var batotoDocument = await parser.ParseAsync(batotoStream);
+
+            /*Batoto has the list of all the pages with links in a drop down*/
+            //Get the select node which contains all the pages with links
+            var selectNode = batotoDocument.All.Where(n => n.Id == "page_select").Select(n => n).First();   
+            
+            //Iterate through the option nodes and get the links for each pages.
+            for(int i = 0; i < selectNode.Children.Count(); i++)
+            {
+                //Create an instance of a BatotoPage
+                BatotoPage myPage = new BatotoPage();
+
+                //set the link
+                myPage.PageUrl = selectNode.Children[i].Attributes["value"].Value;
+
+                //set the page number
+                myPage.PageIndex = i + 1;
+
+                //Add to the list of pages.
+                Pages.Add(myPage);
+            }
+            
+            //Get the Ul Node that contains everthin in the frame
+            var UlNode = selectNode.ParentElement.ParentElement;
+
+            //Get the tilte of the manga inside the inner html of the 1st li node
+            string title = UlNode.Children[0].TextContent;
+            MangaTitle = title;
+
+            //Get the title of the chapter inside the select box of the 2nd li node.
+            var chapterSelectNode = UlNode.Children[1].Children[0];           
+            var selectedchapterNode = (from n in chapterSelectNode.Children
+                               where (n as IHtmlOptionElement).IsSelected == true
+                               select n).First();
+            string chapterTitle = selectedchapterNode.TextContent;
+            ChapterTitle = chapterTitle;
+        }
     }
 }
---
+++ ProjectMangoV3/MangoEngine/Pages/BatotoPage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoEngine.Pages
+{
+    public class BatotoPage : MangaPage
+    {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+        public BatotoPage() : base()
+        {
+            /*Default Constructors*/
+            SourceName = "Batoto";
+        }
+
+        public BatotoPage(string url) : base(url)
+        {
+            /*Overloaded Constructors, accepts a string of URL*/
+            SourceName = "Batoto";
+        }
+
+
+        #endregion
+
+        #region Methods
+
+        public override async Task<string> GetImageUrlAsync()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+    }
+}
