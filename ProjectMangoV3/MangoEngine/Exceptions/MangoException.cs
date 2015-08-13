8fe5f185f99e81b25369da8681e1c563dcdad3d8
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-08-13 15:09:35 +0700

    Done with MangoException

---
+++ ProjectMangoV3/MangoEngine/Exceptions/MangoException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoEngine.Exceptions
+{
+    public class MangoException : Exception
+    {
+        /*Represent the exception of the engine */
+
+        #region Constructor
+        /*Constructors*/
+        public MangoException()
+            : base()
+        {
+
+        }
+
+        public MangoException(string message)
+            : base(message)
+        {
+
+        }
+
+        public MangoException(string message, Exception inner)
+            : base(message, inner)
+        {
+
+        }
+        #endregion
+    }
+}
