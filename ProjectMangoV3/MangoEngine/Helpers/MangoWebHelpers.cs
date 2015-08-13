92d7926e4e08608d7fe06261b33401f056253453
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-08-13 17:00:53 +0700

    Finish with WebHelpers

--- ProjectMangoV3/MangoEngine/Helpers/MangoWebHelpers.cs
+++ ProjectMangoV3/MangoEngine/Helpers/MangoWebHelpers.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MangoEngine.Helpers
 {
-    class MangoWebHelpers
+    public static class MangoWebHelpers
     {
+        /*Methods*/
+        public static string GetCompression(HttpResponseMessage responseMessage)
+        {
+            /*GEt the Compression type of the Website fro the HttpResponseMessage*/
+            if (responseMessage.Content.Headers.ContentEncoding.Count != 0)
+            {
+                return responseMessage.Content.Headers.ContentEncoding.ElementAt(0);
+            }
+
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        public static async Task<Stream> GetStreamAsync(HttpClient myClient, string url, TimeSpan retryInterval, int retryCount = 3)
+        {
+            /*Get the stream to the Website with automatic retry*/
+            var exceptions = new List<Exception>();
+
+            for (int retry = 0; retry < retryCount; retry++)
+            {
+                try
+                {
+                    var stream = await myClient.GetStreamAsync(url);
+
+                    return stream;
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                    await Task.Delay(retryInterval);
+                }
+            }
+
+            throw new AggregateException(exceptions);
+        }
     }
 }
