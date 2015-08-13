cbb7778230b37ef8e941616e4a910205d88bfa0a
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-08-13 16:10:52 +0700

    Fields and properties for the MangaPage

--- ProjectMangoV3/MangoEngine/Pages/MangaPage.cs
+++ ProjectMangoV3/MangoEngine/Pages/MangaPage.cs
@@ -10,10 +10,38 @@ namespace MangoEngine.Pages
     {
         #region Fields;
         /*Fields*/
+        private string _pageUrl;
+        private string _imgUrl;
+
         #endregion
 
         #region Property
         /*Property*/
+        public string PageUrl
+        {
+            get
+            {
+                return _pageUrl;
+            }
+
+            protected set
+            {
+                _pageUrl = value;
+            }
+        }
+
+        public string ImgUrl
+        {
+            get
+            {
+                return _imgUrl;
+            }
+
+            protected set
+            {
+                _imgUrl = value;
+            }
+        }
         #endregion
 
         #region Constructors
