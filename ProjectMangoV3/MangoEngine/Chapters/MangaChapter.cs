3721a8052f5673cc88efbd4e3ccc378e56e06329
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-08-13 16:59:02 +0700

    Add in WebHelpers

--- ProjectMangoV3/MangoEngine/Chapters/MangaChapter.cs
+++ ProjectMangoV3/MangoEngine/Chapters/MangaChapter.cs
@@ -1,13 +1,15 @@
 ﻿using MangoEngine.Pages;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MangoEngine.Chapters
 {
-    public class MangaChapter
+    public abstract class MangaChapter
     {
         /*Represent a chapter of a manga*/
 
@@ -99,15 +101,68 @@ namespace MangoEngine.Chapters
             protected set { _pages = value; }
         }
 
-
         #endregion
 
         #region Constructors
         /*Constructors*/
+        protected MangaChapter()
+        {
+            /*Default Constructor, Accepts no parameter*/
+            _sourceName = string.Empty;
+            _mangaTitle = string.Empty;
+            _chapterTitle = string.Empty;
+            _pagesCount = -1;
+            _baseUrl = string.Empty;
+            _pages = new List<MangaPage>();
+        }
+
+        protected MangaChapter(string url): this()
+        {
+            /*Overloaded Constructor. Accepts a string of URL for a manga chapter.*/
+            _baseUrl = url;
+        }
         #endregion
 
         #region Methods
         /*Methods*/
+        internal abstract Task InitAsync();
+
+        public virtual string GetCompression(HttpResponseMessage responseMessage)
+        {
+            /*GEt the Compression type of the Website fro the HttpResponseMessage*/
+            if (responseMessage.Content.Headers.ContentEncoding.Count != 0)
+            {
+                return responseMessage.Content.Headers.ContentEncoding.ElementAt(0);
+            }
+
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        protected virtual async Task<Stream> GetStreamAsync(HttpClient myClient, string url, TimeSpan retryInterval, int retryCount = 3)
+        {
+            /*Get the stream to the Website with automatic retry*/
+            var exceptions = new List<Exception>();
+
+            for (int retry = 0; retry < retryCount; retry++)
+            {
+                try
+                {
+                    var stream = await myClient.GetStreamAsync(url);
+
+                    return stream;
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                    await Task.Delay(retryInterval);
+                }
+            }
+
+            throw new AggregateException(exceptions);
+        }
         #endregion
     }
 }
