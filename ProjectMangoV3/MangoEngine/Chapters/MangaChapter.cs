d5842fb30370875494c14fe129263470892be2d9
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-08-13 15:12:17 +0700

    Skeleton for MangaChapter

---
+++ ProjectMangoV3/MangoEngine/Chapters/MangaChapter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoEngine.Chapters
+{
+    public class MangaChapter
+    {
+        #region Fields;
+        /*Fields*/
+        #endregion
+
+        #region Property
+        /*Property*/
+        #endregion
+
+        #region Constructors
+        /*Constructors*/
+        #endregion
+
+        #region Methods
+        /*Methods*/
+        #endregion
+    }
+}
