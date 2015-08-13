4fb8175c28cd956d5372a0d020b42ebc212bcd06
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-08-13 21:52:13 +0700

    Finish with the GetImageUrlAsync()

--- ProjectMangoV3/MangoEngine/Pages/BatotoPage.cs
+++ ProjectMangoV3/MangoEngine/Pages/BatotoPage.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using MangoEngine.Helpers;
+using MangoEngine.Exceptions;
+using AngleSharp;
+using AngleSharp.Parser.Html;
+using AngleSharp.Dom;
+using AngleSharp.Dom.Html;
 
 namespace MangoEngine.Pages
 {
@@ -34,7 +42,46 @@ namespace MangoEngine.Pages
 
         public override async Task<string> GetImageUrlAsync()
         {
-            throw new NotImplementedException();
+            /*Get the image url of the page inside the current url asynchronously*/
+            if (!string.IsNullOrEmpty(ImgUrl))
+            {
+                return ImgUrl;
+            }
+
+            //Initialize the client
+            HttpClient myClient = new HttpClient();
+
+            //Get the stream to the website. 
+            Stream batotoStream = null;
+
+            try
+            {
+                batotoStream = await MangoWebHelpers.GetStreamAsync(myClient, PageUrl, new TimeSpan(0, 0, 3));
+            }
+
+            catch (Exception e)
+            {
+                throw new MangoException("GetImageUrl failed! Can't get Stream to the Website!", e);
+            }
+
+            //Create the parser 
+            HtmlParser myParser = new HtmlParser();
+
+            //Parse the HTML
+            var document = await myParser.ParseAsync(batotoStream);
+
+            /* Batoto holds the page's image url inside the img node with the id "comic_page" */
+            //Get the img node.   
+            var imgNode = document.Images.Where(n => n.Id == "comic_page").Select(n => n).First();
+
+            //Get the link
+            string imgLink = imgNode.Source;
+
+            //Set the imgUrl for the quick reference later.
+            ImgUrl = imgLink;
+
+            //return the result
+            return ImgUrl;
         }
         #endregion
 
