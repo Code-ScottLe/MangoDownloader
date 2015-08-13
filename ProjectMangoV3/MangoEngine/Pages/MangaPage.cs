649934dc4e30586cdf091b6f16f814fbad9f87e8
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-08-13 15:23:33 +0700

    Add MangaPage and the skeleton code

---
+++ ProjectMangoV3/MangoEngine/Pages/MangaPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoEngine.Pages
+{
+    public class MangaPage
+    {
+        #region Fields;
+        /*Fields*/
+        #endregion
+
+        #region Property
+        /*Property*/
+        #endregion
+
+        #region Constructors
+        /*Constructors*/
+        #endregion
+
+        #region Methods
+        /*Methods*/
+        #endregion
+    }
+}
