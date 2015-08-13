f655d23ce6ba8f8cef18efec18fc69afce4c5a74
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-08-13 15:11:21 +0700

    Skeleton for BatotoChapter.

---
+++ ProjectMangoV3/MangoEngine/Chapters/BatotoChapter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Parser.Html;
+using System.IO;
+
+namespace MangoEngine.Chapters
+{
+    public class BatotoChapter
+    {
+        #region Fields;
+        /*Fields*/
+        #endregion
+
+        #region Property
+        /*Property*/
+        #endregion
+
+        #region Constructors
+        /*Constructors*/
+        #endregion
+
+        #region Methods
+        /*Methods*/
+        #endregion
+    }
+}
