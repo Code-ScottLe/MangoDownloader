10c3a626bf37d26a82190b3dbcc9c5b1a296d79d
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-09-10 01:27:38 -0700

    Add in DownloadHelper

---
+++ ProjectMangoV3/HttpDownloader/HttpDownloadHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
+
+namespace HttpDownloader
+{
+    public static class HttpDownloadHelper
+    {
+        public static async Task<Stream> GetStreamAsync(HttpClient myClient, string url, TimeSpan retryInterval, int retryCount = 3)
+        {
+            /*Get the stream to the Website with automatic retry*/
+            var exceptions = new List<Exception>();
+
+            for (int retry = 0; retry < retryCount; retry++)
+            {
+                try
+                {
+                    var stream = await myClient.GetStreamAsync(url);
+
+                    return stream;
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                    await Task.Delay(retryInterval);
+                }
+            }
+
+            throw new AggregateException(exceptions);
+        }
+    }
+}
