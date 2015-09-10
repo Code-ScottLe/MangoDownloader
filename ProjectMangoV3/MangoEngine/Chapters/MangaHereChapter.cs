5b6e18592c3288a5485ca13ef91434ea78bf0e2b
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-09-09 20:57:22 -0700

    Half Init for MangaHereChapter

--- ProjectMangoV3/MangoEngine/Chapters/MangaHereChapter.cs
+++ ProjectMangoV3/MangoEngine/Chapters/MangaHereChapter.cs
@@ -9,21 +9,24 @@ using AngleSharp;
 using AngleSharp.Parser.Html;
 using AngleSharp.Dom;
 using AngleSharp.Dom.Html;
+using System.Net.Http;
+using System.IO;
+using System.IO.Compression;
 
 namespace MangoEngine.Chapters
 {
     public class MangaHereChapter : MangaChapter
     {
         #region Fields
-        /**/
+        /*Fields*/
         #endregion
 
-        #region Property
-        /**/
+        #region Properties
+        /*Properties*/
         #endregion
 
         #region Constructors
-        /**/
+        /*Constructors*/
         public MangaHereChapter() : base()
         {
             //default constructor, call base
@@ -39,9 +42,49 @@ namespace MangoEngine.Chapters
         #endregion
 
         #region Methods
-        /**/
+        /*Methods*/
         public override async Task InitAsync()
         {
+            /*Initialize the current instance of MangaChapter to the MangaHere standard.*/
+
+            //Initialize the client for Web purpose
+            HttpClient myClient = new HttpClient();
+
+            //set the timeout of the client (30 secs)
+            myClient.Timeout = new TimeSpan(0, 0, 30);
+
+            //Try to get the stream to the website.
+            Stream mangaHereStream = null;
+
+            try
+            {
+                mangaHereStream = await MangoWebHelpers.GetStreamAsync(myClient, BaseUrl, new TimeSpan(0, 0, 3));
+            }
+
+            catch (Exception e)
+            {
+                throw new MangoException("Init failed! Can't get the stream from the website", e);
+            }
+
+            /*Parsing part*/          
+
+            /*Initialize the parser*/
+            HtmlParser parser = new HtmlParser();
+
+            //Check if the stream is compressed, MangaHere uses random GZip at times.
+            if (MangoWebHelpers.GetCompression(await myClient.GetAsync(BaseUrl)) == "GZip")
+            {
+                //stream is compressed, decompress stream.
+                mangaHereStream = new GZipStream(mangaHereStream,CompressionMode.Decompress);
+            }
+
+            //Parse the document
+            var mangaHereDocument = await parser.ParseAsync(mangaHereStream);
+
+            /*MangaHere has the list of all the pages with links in a drop down*/
+            //Get the select node wich contains all the pages with links
+            
+
 
         }
         #endregion
