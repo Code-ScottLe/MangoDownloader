e874e55b1d934fe1341c64ef6cc0270cba5d8c90
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-10-17 23:29:45 -0700

    SkeletonCode for mangaherePage (2)

--- ProjectMangoV3/MangoEngine/Pages/MangaHerePage.cs
+++ ProjectMangoV3/MangoEngine/Pages/MangaHerePage.cs
@@ -8,5 +8,35 @@ namespace MangoEngine.Pages
 {
     public class MangaHerePage : MangaPage
     {
+        #region Fields
+        /*Fields*/
+        #endregion
+
+        #region Properties
+        /*Properties*/
+        #endregion
+
+        #region Constructors
+        /*Constructors*/
+        public MangaHerePage() : base()
+        {
+            //default constructor
+            SourceName = "MangaHere";
+        }
+
+        public MangaHerePage(string url) : base(url)
+        {
+            //Overloaded constructor, accept a string of url
+            SourceName = "MangaHere";
+        }
+        #endregion
+
+        #region Methods
+        /*Methods*/
+        public override async Task<string> GetImageUrlAsync()
+        {
+            //Get the image URL from the current URL. 
+        }
+        #endregion
     }
 }
