870a61cb890f63c85b6fd579eb2b56c98ee2f3e4
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-10-19 02:25:43 -0700

    Done with GetImageAsync for MangaHerePage

--- ProjectMangoV3/MangoEngine/Pages/MangaHerePage.cs
+++ ProjectMangoV3/MangoEngine/Pages/MangaHerePage.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+using System.IO.Compression;
+using MangoEngine.Helpers;
+using AngleSharp.Parser.Html;
+using AngleSharp.Dom;
+using AngleSharp.Dom.Html;
+using MangoEngine.Exceptions;
 
 namespace MangoEngine.Pages
 {
@@ -36,6 +44,44 @@ namespace MangoEngine.Pages
         public override async Task<string> GetImageUrlAsync()
         {
             //Get the image URL from the current URL. 
+
+            //Create the HttpClient
+            HttpClient myClient = new HttpClient();
+
+            //Try to get the Stream to the Website.
+            //MangaHere is known for their compressed skills.
+            KeyValuePair<Stream, string> temp = await MangoWebHelpers.GetCompressedStreamAsync(myClient, PageUrl, new TimeSpan(0, 0, 5));
+
+            Stream mangaHereStream = temp.Key;
+
+            //Check if the stream is compressed or not.
+            if (temp.Value == "GZip")
+            {
+                //stream is compressed.
+                mangaHereStream = new GZipStream(mangaHereStream, CompressionMode.Decompress);
+            }
+
+            //Create parser
+            HtmlParser myParser = new HtmlParser();
+
+            //Parse the document
+            IHtmlDocument mangaHereDocument = await myParser.ParseAsync(mangaHereStream);
+
+            //Get the image node which has the id == "image";
+            var imgNode = mangaHereDocument.Images.Where(n => n.Id == "image").Select(n => n).First();
+
+
+            //check if we actually get it.
+            if(imgNode == null)
+            {
+                throw new MangoException("Can't get image node that containt img link");
+            }
+
+            //set the ImgUrl
+            ImgUrl = imgNode.Source;
+
+            //return.
+            return ImgUrl;
         }
         #endregion
     }
