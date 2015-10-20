e5e64e34bda5c4380713733f7844307630e2740f
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-10-19 22:57:32 -0700

    Add in mangaHereChapter Test

---
+++ ProjectMangoV3/MangoEngineUnitTest/ChapterTests/MangaHereChapterTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using MangoEngine.Chapters;
+
+namespace MangoEngineUnitTest.ChapterTests
+{
+    public class MangaHereChapterTests
+    {
+    }
+}
