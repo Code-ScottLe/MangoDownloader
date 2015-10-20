cfea1964fc7acbf04f3954eb5352a10398e24a41
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-10-19 17:51:01 -0700

    Add in UnitTest for the Engine

---
+++ ProjectMangoV3/MangoEngineUnitTest/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace MangoEngineUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
