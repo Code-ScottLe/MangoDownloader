271f72f7f3031ff31b4b196bfedd5ec49f6b6006
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-10-19 19:01:44 -0700

    mangaHerePage Test for GetImageUrl done.

--- ProjectMangoV3/MangoEngineUnitTest/PageTests/MangaHerePageTests.cs
+++ ProjectMangoV3/MangoEngineUnitTest/PageTests/MangaHerePageTests.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using MangoEngine.Pages;
 using System.Threading.Tasks;
 
 namespace MangoEngineUnitTest.PageTests
 {
-    class MangaHerePageTests
+    [TestClass]
+    public class MangaHerePageTests
     {
+        [TestMethod]
+        public async Task GetImgUrlTest()
+        {
+            /*Test for the returned IMG Link from the page*/
+
+            //Create an instance of the page.
+            MangaHerePage testPage = new MangaHerePage("http://www.mangahere.co/manga/to_love_ru_darkness/c060/");
+
+            //Try to get the link
+            string imgUrl = await testPage.GetImageUrlAsync();
+
+            string imgUrlActual = "http://a.mhcdn.net/store/manga/8471/060.0/compressed/v001.jpg?v=1444405923";
+
+            //Compare
+            Assert.AreEqual(imgUrl, imgUrlActual);
+        }
     }
 }
