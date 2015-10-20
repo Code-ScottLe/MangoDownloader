e87e0b1820ebd8540cdf88f45a872a43b80a731a
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-10-19 19:45:22 -0700

    Finish with Init Test for BatotoChapter Tests.

--- ProjectMangoV3/MangoEngineUnitTest/ChapterTests/BatotoChapterTests.cs
+++ ProjectMangoV3/MangoEngineUnitTest/ChapterTests/BatotoChapterTests.cs
@@ -14,7 +14,55 @@ namespace MangoEngineUnitTest.ChapterTests
         [TestMethod]
         public async Task InitializationTest()
         {
+            /*Test object intialization*/
 
+            //Initialize the chapter.
+            BatotoChapter testChapter = new BatotoChapter("http://bato.to/read/_/304588/isuca_v6_ch35_by_sacred-blade-scans");
+            await testChapter.InitAsync();
+
+            //Verify the content.
+            string sourceNameActual = "Batoto";
+            string mangaTitleActual = "Isuca";
+            string chapterTitleActual = "Vol.6 Ch.35";
+            int pageCountActual = 28;
+
+            //Test
+            //Source Name
+            SourceNameCheckTest(testChapter, sourceNameActual);
+
+            //Manga Title
+            MangaTitleCheckTest(testChapter, mangaTitleActual);
+
+            //Chapter Title
+            ChapterTitleCheckTest(testChapter, chapterTitleActual);
+
+            //Page Count
+            PageCountCheckTest(testChapter, pageCountActual);
+
+        }
+
+        [TestMethod]
+        private void SourceNameCheckTest(BatotoChapter testChapter, string actualSourceName)
+        {
+            Assert.AreEqual(testChapter.SourceName, actualSourceName);
+        }
+
+        [TestMethod]
+        private void MangaTitleCheckTest(BatotoChapter testChapter, string actualMangaTitle)
+        {
+            Assert.AreEqual(testChapter.MangaTitle, actualMangaTitle);
+        }
+
+        [TestMethod]
+        private void ChapterTitleCheckTest(BatotoChapter testChapter, string actualChapterTitle)
+        {
+            Assert.AreEqual(testChapter.ChapterTitle, actualChapterTitle);
+        }
+
+        [TestMethod]
+        private void PageCountCheckTest(BatotoChapter testChapter, int actualPageCount)
+        {
+            Assert.AreEqual(testChapter.PageCount, actualPageCount);
         }
     }
 }
