b85207c756c43a4148f23680f2adb6a673cf50b9
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-10-20 11:44:32 -0700

    Finish implementing the interface for the chapter.

--- ProjectMangoV3/MangoEngine/Chapters/IMangaChapter.cs
+++ ProjectMangoV3/MangoEngine/Chapters/IMangaChapter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MangoEngine.Pages;
 
 namespace MangoEngine.Chapters
 {
@@ -10,6 +11,12 @@ namespace MangoEngine.Chapters
     {
         #region Properties
         /*Properties*/
+        string SourceName { get; set; }
+        string MangaTitle { get; set; }
+        string ChapterTitle { get; set; }
+        int PageCount { get; set; }
+        string BaseUrl { get; set; }
+        List<IMangaPage> Pages { get; set; }
         #endregion
 
         #region Method
