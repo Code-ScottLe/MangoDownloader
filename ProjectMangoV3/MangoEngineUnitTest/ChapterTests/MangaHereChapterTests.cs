5c60a2e7311b3576c9a562fbef8120f34c884ace
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-10-19 23:03:55 -0700

    Init test for MangaHere is completed

--- ProjectMangoV3/MangoEngineUnitTest/ChapterTests/MangaHereChapterTests.cs
+++ ProjectMangoV3/MangoEngineUnitTest/ChapterTests/MangaHereChapterTests.cs
@@ -8,7 +8,38 @@ using MangoEngine.Chapters;
 
 namespace MangoEngineUnitTest.ChapterTests
 {
+    [TestClass]
     public class MangaHereChapterTests
     {
+
+        [TestMethod]
+        public async Task InitializationTest()
+        {
+            /*Test Object Intialization*/
+
+            //Initlaize the object
+            MangaHereChapter testChapter = new MangaHereChapter("http://www.mangahere.co/manga/d_frag/v04/c074/");
+            await testChapter.InitAsync();
+
+            //Verify the content.
+            string sourceNameActual = "MangaHere";
+            string mangaTitleActual = "D-Frag";
+            string chapterTitleActual = "Vol 04 Ch 074: I Should've Opened It...";
+            int pageCountActual = 16;
+
+            //Test
+            //Source Name
+            Assert.AreEqual(testChapter.SourceName, sourceNameActual);
+
+            //Manga Title
+            Assert.AreEqual(testChapter.MangaTitle, mangaTitleActual);
+
+            //Chapter Title
+            Assert.AreEqual(testChapter.ChapterTitle, chapterTitleActual);
+
+            //Page Count
+            Assert.AreEqual(testChapter.PageCount, pageCountActual);
+        }
+        
     }
 }
