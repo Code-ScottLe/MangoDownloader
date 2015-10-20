af7a1bb6fe3840556d5c03ac38060732bef7c4a4
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-10-19 19:05:19 -0700

    Add in batotoChapter tests.

---
+++ ProjectMangoV3/MangoEngineUnitTest/ChapterTests/BatotoChapterTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using MangoEngine.Chapters;
+
+namespace MangoEngineUnitTest.ChapterTests
+{
+    [TestClass]
+    public class BatotoChapterTests
+    {
+        [TestMethod]
+        public async Task InitializationTest()
+        {
+
+        }
+    }
+}
