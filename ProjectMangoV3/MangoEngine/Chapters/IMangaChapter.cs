cdadab183b7b678e67922a9925f71b704c672f11
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-10-20 11:35:45 -0700

    Add in Interface common for the Chapter.

---
+++ ProjectMangoV3/MangoEngine/Chapters/IMangaChapter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoEngine.Chapters
+{
+    public interface IMangaChapter
+    {
+        #region Properties
+        /*Properties*/
+        #endregion
+
+        #region Method
+        /*Methods*/
+        #endregion
+
+    }
+}
