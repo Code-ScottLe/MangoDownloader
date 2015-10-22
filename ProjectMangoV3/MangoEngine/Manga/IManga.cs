6a1f31d5f0ce2f456ef8230772d8beabed218425
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-10-21 23:52:47 -0700

    Add in Manga (Whole) for the Engine.

---
+++ ProjectMangoV3/MangoEngine/Manga/IManga.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoEngine.Manga
+{
+    public interface IManga
+    {
+        /*Generally describe a manga from a manga source.*/
+
+        #region Properties
+        /*Properties*/
+        #endregion
+
+        #region Methods
+        /*Methods*/
+        #endregion
+
+    }
+}
