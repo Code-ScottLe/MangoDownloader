4b805ee73d3aba7f5e8c3e94504cf85a0c09ab75
Vu Ngoc Le (Scott Le) <amraammissiles@hotmail.com>
2015-10-21 23:55:53 -0700

    Add in skeleton code for Manga Abstract Base class.

---
+++ ProjectMangoV3/MangoEngine/Manga/Manga.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangoEngine.Manga
+{
+    public abstract class Manga : IManga
+    {
+        /*Abstract base class that represent a manga from a source.*/
+        #region Fields
+        /*Fields*/
+        #endregion
+
+        #region Properties
+        /*Properties*/
+        #endregion
+
+        #region Constructors
+        /*Constructors*/
+        #endregion
+
+        #region Methods
+        /*Methods*/
+        #endregion
+    }
+}
